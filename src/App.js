27be6e5714befa6b9f515a7a775a8e2ee77768f9
linruhui <48901635+linruhui@users.noreply.github.com>
2021-05-24 13:42:49 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import "./index.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+  function myfunc(){
+    fetch('https://linru-testcookie-func.azurewebsites.net/api/testsetcookie', {credentials: 'include'});
+    console.log('The link was clicked.');
+  };
+ 
+
+export default function App() {
+  return (
+    <div className="App">
+      <button onClick={myfunc}>get cookies</button>
+      <h2 id="cookievalue">Cookie value here</h2>
+    </div>
+  );
 }
-
-export default App;
