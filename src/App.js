2faba61fce45a4d8b0db95f8305fdaec7e73b85e
Linru Hui <linhui@microsoft.com>
2021-09-30 10:07:54 +0800

    add api to swa:

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import React from 'react';
-import "./index.css";
-
-
-  function myfunc(){
-    fetch('https://linru-testcookie-func.azurewebsites.net/api/testsetcookie', {credentials: 'include'});
-    console.log('The link was clicked.');
-  };
- 
-
-export default function App() {
-  return (
-    <div className="App">
-      <button onClick={myfunc}>get cookies</button>
-      <h2 id="cookievalue">Cookie value here</h2>
-    </div>
-  );
-}
+module.exports = async function (context, req) {
+  context.res.json({
+      text: "Hello from the API"
+  });
+};
