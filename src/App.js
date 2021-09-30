79b276c40d79b2a884efff20b5d54f0f3c958a9e
Linru Hui <linhui@microsoft.com>
2021-09-30 10:39:51 +0800

    Revert "add api to swa:"
    This reverts commit 2faba61fce45a4d8b0db95f8305fdaec7e73b85e.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-module.exports = async function (context, req) {
-  context.res.json({
-      text: "Hello from the API"
-  });
-};
+import React from 'react';
+import "./index.css";
+
+
+  function myfunc(){
+    fetch('https://linru-testcookie-func.azurewebsites.net/api/testsetcookie', {credentials: 'include'});
+    console.log('The link was clicked.');
+  };
+ 
+
+export default function App() {
+  return (
+    <div className="App">
+      <button onClick={myfunc}>get cookies</button>
+      <h2 id="cookievalue">Cookie value here</h2>
+    </div>
+  );
+}
